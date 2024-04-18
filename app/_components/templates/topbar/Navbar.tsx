d5fc48377c86b3b57f72a6d0d1437b66d18afe0f
mohammadreza <spd.mohammadreza@gmail.com>
2024-04-18 09:22:22 -0700

    add btn lang

--- app/_components/templates/topbar/Navbar.tsx
+++ app/_components/templates/topbar/Navbar.tsx
@@ -1,17 +1,43 @@
-import React from "react";
+"use client";
+import React, { ChangeEvent } from "react";
+import AdsMd from "./AdsMd";
+import { useRouter } from "next/navigation";
+import { useLocale } from "next-intl";
 
 export default function Navbar() {
+  const router = useRouter();
+  const localActive = useLocale();
+  const onSelectChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    const nextLocale = e.target.value;
+    router.replace(`/${nextLocale}`);
+  };
   return (
-    <div className="sticky top-6 w-full mt-6 flex justify-between items-center px-5">
-      <div className="">
+    <div className="sticky top-6 w-full  flex justify-between  px-5">
+      <div className="mt-6 md:hidden">
         <img src="/navbar/hamber.svg" alt="" />
       </div>
-      <div>
-        <img src="/navbar/icon.png" alt="" />
+      <div className="flex gap-4">
+        <div className="mt-6">
+          <img src="/navbar/icon.png" alt="" />
+        </div>
+        <AdsMd></AdsMd>
       </div>
-      <div className="rounded-full size-10 bg-white/10 flex justify-center items-center">
-        <div className="rounded-full size-7 bg-background flex justify-center items-center">
-        <img src="/navbar/user.svg" alt="" />
+      <div className="flex items-center gap-3 mt-6 h-fit">
+        <select
+          defaultValue={localActive}
+          onChange={onSelectChange}
+          className="bg-background outline-none hidden md:block "
+          name=""
+          id=""
+        >
+          <option value="fa">Fa</option>
+          <option value="en">En</option>
+        </select>
+        <span className="w-px h-7 bg-white/15 hidden md:block"></span>
+        <div className="  rounded-full size-10 bg-white/10 flex justify-center items-center">
+          <div className="rounded-full size-7 bg-background flex justify-center items-center">
+            <img src="/navbar/user.svg" alt="" />
+          </div>
         </div>
       </div>
     </div>
