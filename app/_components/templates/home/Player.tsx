6f5d8d8ca362d41d5fde19aa80eed9a6d8918c3e
mohammadreza <spd.mohammadreza@gmail.com>
2024-04-20 04:37:46 -0700

    video

--- app/_components/templates/home/Player.tsx
+++ app/_components/templates/home/Player.tsx
@@ -4,7 +4,7 @@ export default function Player() {
   return (
     <>
       <div className="mid flex justify-center">
-        <div className="mt-11 w-[820px] rounded-[60px] overflow-hidden relative">
+        <div className="mt-11 w-[820px] rounded-3xl md:rounded-[60px] overflow-hidden relative">
           <img className="blur-sm" src="/home/video.png" alt="" />
           <div className="absolute top-0 left-0 bg-black/60 w-full h-full"></div>
           <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 size-9">
