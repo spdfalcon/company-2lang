36b9fdc2153e9bea06e80a88d9f98c4282234e05
mohammadreza <spd.mohammadreza@gmail.com>
2024-07-17 21:40:50 -0700

    fix some bug

--- app/_components/modules/SocialMedia.tsx
+++ app/_components/modules/SocialMedia.tsx
@@ -5,9 +5,9 @@ import React from 'react'
 const SocialMedia:React.FC<SocialmediaProps> = ({title , bg , iconsm , iconmd}) => {
   return (
     <div className=''>
-      <div className={`relative w-[330px] h-[70px] ps-5 flex items-center  ${bg}  rounded-[18px]`}>
+      <div className={`relative w-[280px] md:w-[330px] h-[60px] md:h-[70px] ps-5 flex items-center  ${bg}  rounded-[18px]`}>
         <span className={`text-sm`} >{title}</span>
-        <Image className='-translate-y-5' alt='' src={iconsm} width={101} height={101}></Image>
+        <Image className='-translate-y-5' alt='' src={iconsm} width={90} height={90}></Image>
         <Image className='absolute top-1/2 -translate-y-1/2 -left-3 ' alt='' src={iconmd} width={28} height={28}></Image>
       </div>
     </div>
--- app/_components/templates/home/HamberMenu.tsx
+++ app/_components/templates/home/HamberMenu.tsx
@@ -3,28 +3,56 @@ import Link from "next/link";
 import React from "react";
 import SelectLang from "../topbar/SelectLang";
 import { useLocale, useTranslations } from "next-intl";
+import { usePathname } from "next/navigation";
 
 export default function HamberMenu({ setIsShow }: any) {
+  const navLinks = [
+    { id: 1, title: "mainpage", href: "/" },
+    { id: 2, title: "portfolio", href: "/portfolio" },
+    { id: 3, title: "websitedesignfee", href: "/Tariff" },
+    { id: 4, title: "articles", href: "/articles" },
+    { id: 5, title: "aboutus", href: "/aboutus" },
+    { id: 6, title: "contactus", href: "/contactus" },
+  ];
   const t = useTranslations("menu");
   const localActive = useLocale();
+  const pathname = usePathname();
 
   return (
-        <div className="px-8 py-12 bg-black w-full h-screen">
-          <div className="flex justify-between w-full">
-            <Image
-              alt=""
-              src={"/footer/icon.png"}
-              width={65.52}
-              height={43.71}
-            ></Image>
-            <i
-              onClick={() => setIsShow((priv: any) => !priv)}
-              className="bi bi-x-lg cursor-pointer p-1"
-            ></i>
-          </div>
-          <div>
-            <div className="flex flex-col gap-9 mt-12 text-xs">
+    <div className="px-8 py-12 bg-black w-full h-screen">
+      <div className="flex justify-between w-full">
+        <Image
+          alt=""
+          src={"/footer/icon.png"}
+          width={65.52}
+          height={43.71}
+        ></Image>
+        <i
+          onClick={() => setIsShow((priv: any) => !priv)}
+          className="bi bi-x-lg cursor-pointer p-1"
+        ></i>
+      </div>
+      <div>
+        <ul className="flex flex-col gap-9 mt-12 text-sm">
+          {navLinks.map((link) => (
+            <li className="cursor-pointer" key={link.id}>
               <Link
+                onClick={() => setIsShow((priv: any) => !priv)}
+                className={
+                  pathname === `/${localActive}${link.href}`
+                    ? "gradient-orange text-transparent bg-clip-text before:size-[2px] before:gradient-orange before:block relative before:absolute before:-top-px before:-right-1 before:rounded-full "
+                    : pathname === `${link.href}${localActive}`
+                    ? "gradient-orange text-transparent bg-clip-text before:size-[2px] before:gradient-orange before:block relative before:absolute before:-top-px before:-right-1 before:rounded-full "
+                    : ""
+                }
+                href={`/${localActive}${link.href}`}
+              >
+                {t(link.title)}
+              </Link>
+            </li>
+          ))}
+
+          {/* <Link
                 onClick={() => setIsShow((priv: any) => !priv)}
                 href={`/${localActive}/portfolio`}
               >
@@ -53,10 +81,12 @@ export default function HamberMenu({ setIsShow }: any) {
                 href={`/${localActive}/contactus`}
               >
                 {t("contactus")}
-              </Link>
-              <SelectLang></SelectLang>
-            </div>
-          </div>
+              </Link> */}
+        </ul>
+        <div className="mt-5">
+          <SelectLang></SelectLang>
         </div>
+      </div>
+    </div>
   );
 }
