ed6ede517c863b5e77f30f547f65c1b5fc79a91f
mohammadreza <spd.mohammadreza@gmail.com>
2024-07-17 21:02:41 -0700

    end portfolio

--- app/_components/modules/porfolio/PortfolioFilter.tsx
+++ app/_components/modules/porfolio/PortfolioFilter.tsx
@@ -7,8 +7,8 @@ const PortfolioFilter = () => {
   return (
     <div>
     <div className="mt-6 rtl:ps-5 ltr:pe-5">
-      <div className="rtl:rounded-s-lg ltr:rounded-e-lg flex text-nowrap overflow-auto gap-4  *:flex *:items-center *:justify-center bg-white text-second_dark text-xs">
-        <div className="px-9 py-4 gradient-orange rounded-lg text-white font-medium">همه</div>
+      <div className="rtl:rounded-s-lg ltr:rounded-e-lg flex text-nowrap overflow-auto gap-4  *:flex *:items-center *:justify-center bg-second_dark text-white pl-5 ltr:pr-5 text-xs shadow-md shadow-black/65">
+        <div className="px-9 py-4 gradient-orange rounded-lg font-medium">همه</div>
         <div className="">سایت فروشگاهی</div>
         <div>سایت آموزشی</div>
         <div>سایت شرکتی</div>
