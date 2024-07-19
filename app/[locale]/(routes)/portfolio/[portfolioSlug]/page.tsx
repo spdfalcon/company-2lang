76695d4917cea9e6a91c544be5d004b4cf57282c
mohammadreza <spd.mohammadreza@gmail.com>
2024-07-19 09:15:11 -0700

    start portfolioSlug

--- app/[locale]/(routes)/portfolio/[portfolioSlug]/page.tsx
+++ app/[locale]/(routes)/portfolio/[portfolioSlug]/page.tsx
@@ -1,11 +1,67 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
   return (
-    <div>
-      portfolio slug
+    <div className="container mt-16">
+      <div className="bredcromp">
+        <div className="rounded-md p-3 flex items-center gap-2 bg-second_dark text-badge w-fit">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-3"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+            />
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-3"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 19.5 8.25 12l7.5-7.5"
+            />
+          </svg>
+          <span className="text-xs">نمونه کار ها</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-3"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 19.5 8.25 12l7.5-7.5"
+            />
+          </svg>
+          <span className="text-xs">شرکت آزاسیت</span>
+        </div>
+      </div>
+      <div className="mt-32 relative header px-5 flex flex-col items-center gap-6">
+        <h1 className="text-3xl font-cinema">نمونه کار شرکت آزاسیت</h1>
+        <h2 className="absolute -top-10 text-3xl bg-gradient-to-b from-[#FFFFFF] to-[#030712] text-transparent bg-clip-text font-bold opacity-20">
+          Company Azasit
+        </h2>
+      </div>
+      <div className="show-portfolio flex flex-col gap-4">
+        
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
